c993b79f9760a36703c1d31d608f92ee06743e9a
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-24 02:07:29 -0600

    seeding-questions-1-to-36

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
   root to: 'pages#home' #this should be the login page.
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  GET "/companies/:id/company_quizzes/new", to: "company_quizzes#new"
-  POST "/companies/:id/company_quizzes",    to: "company_quizzes#create"
-  GET "/users/:id/stats",                    to: "users#stats"
-  GET "/companies/:id/leaderboard",         to: "comapanies#leaderboard"
-  GET "/home",                              to: "users#home"
-  GET "/quiz_results/new",                  to: "quiz_results#new"
-  POST "/quiz_results",                     to: "quiz_results#create"
-  GET "/subject_areas",                     to: "subject_areas#index"
-  GET "/quiz_results/:id",                  to: "quiz-results#show"
-  GET "/users/:id/rewards",                 to: "users#rewards"
-  GET "/companies/:id/company_quizzes",     to: "company_quizzes#index"
+  get "/companies/:id/company_quizzes/new", to: "company_quizzes#new"
+  post "/companies/:id/company_quizzes",    to: "company_quizzes#create"
+  get "/users/:id/stats",                    to: "users#stats"
+  get "/companies/:id/leaderboard",         to: "comapanies#leaderboard"
+  get "/home",                              to: "users#home"
+  get "/quiz_results/new",                  to: "quiz_results#new"
+  post "/quiz_results",                     to: "quiz_results#create"
+  get "/subject_areas",                     to: "subject_areas#index"
+  get "/quiz_results/:id",                  to: "quiz_results#show"
+  get "/users/:id/rewards",                 to: "users#rewards"
+  get "/companies/:id/company_quizzes",     to: "company_quizzes#index"
   
 end
