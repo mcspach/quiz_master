57175f741bebf63b9d35cfbe0537821892366271
Matthew Spach <matthewspach@gmail.com>
2020-11-25 12:44:22 -0600

    debug start

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -82,6 +82,6 @@ class UsersController < ApplicationController
 
   def home
     @user = current_user
-    @quiz_id = CompanyQuiz.sample.quiz
+    @quiz_id = CompanyQuiz.all.sample.quiz
   end
 end
