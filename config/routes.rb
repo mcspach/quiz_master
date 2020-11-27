c0c4ff1a219a11a74d4e0ced72e80adaa7e14e1d
Matthew Spach <matthewspach@gmail.com>
2020-11-27 15:42:00 -0600

    fixed index route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get "/quiz/:quiz_id/quiz_results/new",    to: "quiz_results#new", as: :quiz_results_new
   post "/quiz_results",                     to: "quiz_results#create"
   get "/subject_areas",                     to: "subject_areas#index", as: :library
-  get "/quiz_results/:id",                  to: "quiz_results#show", as: :results
+  get "/quiz_results",                      to: "quiz_results#index", as: :results
   get "/users/:id/rewards",                 to: "users#rewards", as: :rewards_user
   get "/companies/:id/company_quizzes",     to: "companies#company_quizzes", as: :company_quizzes
 end
