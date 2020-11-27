2cd069f135e424492dd0814ea366699a248f7846
Matthew Spach <matthewspach@gmail.com>
2020-11-27 13:31:54 -0600

    stats page, users controller, rewards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,67 +1,48 @@
 class UsersController < ApplicationController
   def stats
-    @user = current_user
-
-    @current_points = 0
-    @user.quiz_results.each do |result|
-      points = result.score * 100
-      return @current_points += points if result.quiz.quiz_type == "campaign"
-    end
-
-    @current_possible_points = 0
-    @user.quiz_results.each do |possible|
-      possible_points = possible.possible_score * 100
-      return @current_possible_points += possible_points if result.quiz.quiz_type == "campaign"
-    end
-
-    @remaining_points = 3600 - @current_possible_points
-    # This is all for the stats page
 
     # make a score for each subject area in a variable
-    pre_inspection_results = []
-    post_inspection_results = []
-    distracted_driving_results = []
-    proper_procedure_results = []
+    # pre_inspection_results = []
+    # post_inspection_results = []
+    # distracted_driving_results = []
+    # proper_procedure_results = []
     # interate through quiz results
-    QuizResult.each do |result|
-      # make a variable for percentage correct
-      correct_percentage = (result.score / result.possible_score) * 10
-      # find the subject_area of the quiz_result
-      # add the score from that quiz to that variable
-      if result.quiz.subject_area == "Pre-Inspection"
-        pre_inspection_results << correct_percentage
-      elsif result.quiz.subject_area == "Post-Inspection"
-        post_inspection_results << correct_percentage
-      elsif result.quiz.subject_area == "Distracted Driving"
-        distracted_driving_results << correct_percentage
-      else 
-        # result.quiz.subject_area == "Proper Procedure"
-        proper_procedure_results << correct_percentage
-      end
-    end
+    # QuizResult.all.each do |result|
+    #   # make a variable for percentage correct
+    #   correct_percentage = (result.score / result.possible_score) * 10
+    #   # find the subject_area of the quiz_result
+    #   # add the score from that quiz to that variable
+    #   if result.quiz.subject_area == "Pre-Inspection"
+    #     pre_inspection_results << correct_percentage
+    #   elsif result.quiz.subject_area == "Post-Inspection"
+    #     post_inspection_results << correct_percentage
+    #   elsif result.quiz.subject_area == "Distracted Driving"
+    #     distracted_driving_results << correct_percentage
+    #   else 
+    #     # result.quiz.subject_area == "Proper Procedure"
+    #     proper_procedure_results << correct_percentage
+    #   end
+    # end
     # find the score for each subject area
-    @pre_inspection_score = (pre_inspection_results.sum / pre_inspection_results.size)
-    @post_inspection_score = (post_inspection_results.sum / post_inspection_results.size)
-    @distracted_driving_score = (distracted_driving_results.sum / distracted_driving_results.size)
-    @proper_procedure_score = (proper_procedure_results.sum / proper_procedure_results.size)
-    # put subject areas in order of goodness
-    stats_array = [@pre_inspection_score, @post_inspection_score, @distracted_driving_score, @proper_procedure_score].sort
+    # @pre_inspection_score = (pre_inspection_results.sum / pre_inspection_results.size)
+    # @post_inspection_score = (post_inspection_results.sum / post_inspection_results.size)
+    # @distracted_driving_score = (distracted_driving_results.sum / distracted_driving_results.size)
+    # @proper_procedure_score = (proper_procedure_results.sum / proper_procedure_results.size)
+    # # put subject areas in order of goodness
+    # stats_array = [@pre_inspection_score, @post_inspection_score, @distracted_driving_score, @proper_procedure_score].sort
 
-    @best_subject = stats_array.last
-    @worst_subject = stats_array.first
+    # @best_subject = stats_array.last
+    # @worst_subject = stats_array.first
 
     # percentage correct
     percent_array = []
-    QuizResult.each do |result|
-      percent_array << ((result.score / result.possible_score) * 10)
+    QuizResult.all.each do |result|
+      percent_array << (result.score * 100 / result.possible_score)
     end
     @percent_correct = ( percent_array.sum / percent_array.size )
 
-    # percentage incorrect
-    @percent_incorrect = ( 100 - @percent_correct )
-
     # percentage complete
-    @percent_complete = ( QuizResult.where(user: currect_user).size / CompanyQuiz.all.size )
+    @percent_complete = ( QuizResult.where(user: current_user).size * 100 / CompanyQuiz.all.size )
   end
 
   def rewards
@@ -69,20 +50,16 @@ class UsersController < ApplicationController
     @current_points = 0
     @user.quiz_results.each do |result|
       points = result.score * 100
-      return @current_points += points
+      @current_points += points
     end
     @current_possible_points = 0
     @user.quiz_results.each do |possible|
       possible_points = possible.possible_score * 100
-      return @current_possible_points += possible_points
+      @current_possible_points += possible_points
     end
 
-    @remaining_points = 3600 - @current_possible_points
-    @div_points = (@remaining_points / 3600)
-    @current_possible_points = 100
-    @current_points_percentage = 20 # (@current_points * 100) / @current_possible_points
     @remaining_points = (3600 - @current_possible_points)
-
+    @div_points = @remaining_points * 100 / 3600
   end
 
   def home
