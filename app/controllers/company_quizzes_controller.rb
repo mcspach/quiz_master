f2767d509431e7365c3e7d41e6a3756f8cd60c14
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-27 21:08:39 -0600

    created manager quiz selection pages and got twilio working

--- app/controllers/company_quizzes_controller.rb
+++ app/controllers/company_quizzes_controller.rb
@@ -17,9 +17,30 @@ class CompanyQuizzesController < ApplicationController
     @company_quiz.save!
   end
 
+  def safety_refresher; end
+
+  def incident_minimization; end
+
+  def twilio
+    client = Twilio::REST::Client.new
+    client.messages.create({
+                             from: ENV['TWILIO_PHONE_NUMBER'],
+                             to: '+16174485001',
+                             body: "Please take your weekly safety refresher test here"
+                           })
+
+    redirect_to root_path
+  end
+
   private
 
   def company_quiz_params
     params.require(:company_quiz).permit(:quiz_id, :company_id)
   end
+
+
+  def twilio_params
+  params.require(:refresher_topic)
+  end
+
 end
