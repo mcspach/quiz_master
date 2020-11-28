b8c98a6774cac0051342bec3f46d5a5b8b18bb54
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-27 23:51:17 -0600

    Got form to record quiz score and send to Quiz Results

--- app/controllers/quiz_results_controller.rb
+++ app/controllers/quiz_results_controller.rb
@@ -2,31 +2,30 @@ class QuizResultsController < ApplicationController
   def new
     @quiz = Quiz.find(params[:quiz_id])
     @quiz_result = QuizResult.new
+    @hammer = "hammer"
   end
-
   def create
+    @quiz_result = QuizResult.new(quiz_result_params)
     @quiz = Quiz.find(params[:quiz_id])
-    @quiz_result = QuizResult.new
     @quiz_result.user = current_user
     @quiz_result.quiz = @quiz
+    @quiz_result.possible_score = @quiz.questions.count
     if @quiz_result.save!
       redirect_to results_path
     else
       redirect_to home_path
     end
   end
-
   def index
     @user = current_user
     @quiz_results = QuizResult.where(user: @user)
     @todays_quiz = @quiz_results.last
   end
-
   def show
     @quiz_result = QuizResult.find(params[:id])
   end
-
   private 
-
-  params.require(:quiz_result).
+  def quiz_result_params
+    params.require(:quiz_result).permit(:quiz_id, :score, :possible_score, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,14 @@ Rails.application.routes.draw do
   get "/companies/:id/leaderboard",         to: "companies#leaderboard", as: :leaderboard
   get "/home",                              to: "users#home", as: :home
   get "/quiz/:quiz_id/quiz_results/new",    to: "quiz_results#new", as: :quiz_results_new
-  post "/quiz_results",                     to: "quiz_results#create", as: :quiz_results_create
+  # post "/quiz_results/:quiz_id",                    to: "quiz_results#create" 
   get "/subject_areas",                     to: "subject_areas#index", as: :library
   get "/quiz_results",                      to: "quiz_results#index", as: :results
   get "/users/:id/rewards",                 to: "users#rewards", as: :rewards_user
   get "/companies/:id/company_quizzes",     to: "companies#company_quizzes", as: :company_quizzes
+
+resources :quiz do 
+  resources :quiz_results, only: [:create]
+end
+
 end
