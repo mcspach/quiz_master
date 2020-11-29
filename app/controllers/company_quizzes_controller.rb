c9dd609fd6f4c001ddfa1e72ac86288c40f6316c
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-28 18:44:07 -0600

    completed manager safety refresher user story

--- app/controllers/company_quizzes_controller.rb
+++ app/controllers/company_quizzes_controller.rb
@@ -19,15 +19,17 @@ class CompanyQuizzesController < ApplicationController
 
   def incident_minimizer; end
 
+  def quiz_selected; end
+
   def twilio
     client = Twilio::REST::Client.new
     client.messages.create({
                              from: ENV['TWILIO_PHONE_NUMBER'],
                              to: '+16174485001',
-                             body: "Please take your weekly safety refresher test here"
+                             body: text_body
                            })
 
-    redirect_to root_path
+    redirect_to quiz_selected_path
   end
 
   private
@@ -36,9 +38,25 @@ class CompanyQuizzesController < ApplicationController
     params.require(:company_quiz).permit(:quiz_id, :company_id)
   end
 
-
   def twilio_params
-  params.require(:refresher_topic)
+    params.require(:safety_refresher)
+    params.require(:incident_minimizer)
   end
 
+  def form_type
+    return 'safety_refresher' if params[:safety_refresher]
+    return 'incident_minimizer' if params[:incident_minimizer]
+  end
+
+  def text_body
+    url = nil
+
+    if form_type == 'safety_refresher'
+      url = home_url
+    # else
+    #   # 
+    end
+
+    return "Please take your weekly safety refresher test: #{url}"
+  end
 end
